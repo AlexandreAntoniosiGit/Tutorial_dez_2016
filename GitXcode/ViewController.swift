6ead01b2df7f6e16dee879131be76366e183cee5
Jones Carvalho <RodrigoRodrigues@macbook-air-de-rawl.local>
2016-12-20 20:47:10 -0200

    Criação do botão e exibição de alerta

--- GitXcode/ViewController.swift
+++ GitXcode/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func bntAlerta(_ sender: UIButton) {
+        let alerta = UIAlertController(title: "Alerta", message: "Hello World", preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alerta.addAction(action)
+        present(alerta, animated: true, completion: nil)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
